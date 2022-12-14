c296367eb9e97f98d2f8ed6f4a7a17fc7e0c813e
dhani.frans <dhani.francisco.9@gmail.com>
2022-12-14 13:31:00 +0700

    [CREATE MIGRATION] Buat migration rab, detail_rab

---
+++ database/migrations/2022_12_14_062940_create_rab_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rab', function (Blueprint $table) {
+            $table->string("kode_rab", 150)->primary();
+            $table->string("pekerjaan");
+            $table->integer("tahun_anggaran");
+            $table->bigInteger("real_cost");
+            $table->bigInteger("ppn");
+            $table->bigInteger("total_cost");
+            $table->bigInteger("dibulatkan")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rab');
+    }
+};
