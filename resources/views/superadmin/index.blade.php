ab61962beb39675cba9acb509b747736b4147c8f
OkvyanRizkyPramarta <pramarta26@gmail.com>
2022-12-16 12:33:01 +0700

    Halaman Utama SuperAdmin

--- resources/views/superadmin/index.blade.php
+++ resources/views/superadmin/index.blade.php
@@ -3,20 +3,20 @@
 @section('content')
 <!-- Breadcomb area Start-->
     <div class="breadcomb-area" style="padding-top:120px;">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<div class="breadcomb-list">
-						<div class="row">
-							<div class="col-lg-12 col-md-6 col-sm-6 col-xs-12" style="height:100px;margin-top:50px;">
-								<h2 class="center-text" style="text-align: center;">Halaman {{ Auth::user()->name }}</h2>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="breadcomb-list">
+              <div class="row">
+                <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12" style="height:100px;margin-top:50px;">
+                  <h2 class="center-text" style="text-align: center;">Halaman {{ Auth::user()->name }}</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+		  </div>
+	  </div>
     <!-- Breadcomb area Start-->
 	<div class="breadcomb-area">
 		<div class="container">
@@ -60,84 +60,6 @@
         </div>
     </div>
     <!-- Bar Chart area End-->
-	<!-- Breadcomb area End-->
-    <div class="footer-copyright-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="footer-copy-right">
-                        <p>Copyright © 2018 
-. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- End Footer area-->
-    <!-- jquery
-		============================================ -->
-    <script src="{{ asset('admin/js/vendor/jquery-1.12.4.min.js') }}"></script>
-    <!-- bootstrap JS
-		============================================ -->
-    <script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
-    <!-- wow JS
-		============================================ -->
-    <script src="{{ asset('admin/js/wow.min.js') }}"></script>
-    <!-- price-slider JS
-		============================================ -->
-    <script src="{{ asset('admin/js/jquery-price-slider.js') }}"></script>
-    <!-- owl.carousel JS
-		============================================ -->
-    <script src="{{ asset('admin/js/owl.carousel.min.js') }}"></script>
-    <!-- scrollUp JS
-		============================================ -->
-    <script src="{{ asset('admin/js/jquery.scrollUp.min.js') }}"></script>
-    <!-- meanmenu JS
-		============================================ -->
-    <script src="{{ asset('admin/js/meanmenu/jquery.meanmenu.js') }}"></script>
-    <!-- counterup JS
-		============================================ -->
-    <script src="{{ asset('admin/js/counterup/jquery.counterup.min.js') }}"></script>
-    <script src="{{ asset('admin/js/counterup/waypoints.min.js') }}"></script>
-    <script src="{{ asset('admin/js/counterup/counterup-active.js') }}"></script>
-    <!-- mCustomScrollbar JS
-		============================================ -->
-    <script src="{{ asset('admin/js/scrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
-    <!-- sparkline JS
-		============================================ -->
-    <script src="{{ asset('admin/js/sparkline/jquery.sparkline.min.js') }}"></script>
-    <script src="{{ asset('admin/js/sparkline/sparkline-active.js') }}"></script>
-    <!-- flot JS
-		============================================ -->
-    <script src="{{ asset('admin/js/flot/jquery.flot.js') }}"></script>
-    <script src="{{ asset('admin/js/flot/jquery.flot.resize.js') }}"></script>
-    <script src="{{ asset('admin/js/flot/flot-active.js') }}"></script>
-    <!-- knob JS
-		============================================ -->
-    <script src="{{ asset('admin/js/knob/jquery.knob.js') }}"></script>
-    <script src="{{ asset('admin/js/knob/jquery.appear.js') }}"></script>
-    <script src="{{ asset('admin/js/knob/knob-active.js') }}"></script>
-    <!-- Charts JS
-	============================================ -->
-    <script src="{{ asset('admin/js/charts/Chart.js') }}"></script>
-    <script src="{{ asset('admin/js/charts/bar-chart.js') }}"></script>
-    <!--  todo JS
-		============================================ -->
-    <script src="{{ asset('admin/js/todo/jquery.todo.js') }}"></script>
-	<!--  wave JS
-		============================================ -->
-    <script src="{{ asset('admin/js/wave/waves.min.js') }}"></script>
-    <script src="{{ asset('admin/js/wave/wave-active.js') }}"></script>
-    <!-- plugins JS
-		============================================ -->
-    <script src="{{ asset('admin/js/plugins.js') }}"></script>
-    <!-- Data Table JS
-		============================================ -->
-    <script src="{{ asset('admin/js/data-table/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('admin/js/data-table/data-table-act.js') }}"></script>
-    <!-- main JS
-		============================================ -->
-    <script src="{{ asset('admin/js/main.js') }}"></script>
     {{-- 
     <script>
         (function ($) {
