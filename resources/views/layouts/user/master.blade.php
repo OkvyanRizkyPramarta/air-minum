0d498a6cc10d3ea043615a9d2659f05299eee1d6
OkvyanRizkyPramarta <pramarta26@gmail.com>
2022-12-17 10:25:34 +0700

    Disabled Klik Kanan dan Print sc

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -208,8 +208,36 @@ li:hover {
 }
 
 </style>
+<script>
+ document.addEventListener("keyup", function (e) {
+    var keyCode = e.keyCode ? e.keyCode : e.which;
+            if (keyCode == 44) {
+                stopPrntScr();
+            }
+        });
+function stopPrntScr() {
+
+            var inpFld = document.createElement("input");
+            inpFld.setAttribute("value", ".");
+            inpFld.setAttribute("width", "0");
+            inpFld.style.height = "0px";
+            inpFld.style.width = "0px";
+            inpFld.style.border = "0px";
+            document.body.appendChild(inpFld);
+            inpFld.select();
+            document.execCommand("copy");
+            inpFld.remove(inpFld);
+        }
+       function AccessClipboardData() {
+            try {
+                window.clipboardData.setData('text', "Access   Restricted");
+            } catch (err) {
+            }
+        }
+        setInterval("AccessClipboardData()", 300);
+</script>
 </head>
-<body>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
 <!--header section start -->
   <div class="header_section" style="background-image:url({{url('user/images/TelukCenderawasih.jpg')}})">
   <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: transparent !important;">
