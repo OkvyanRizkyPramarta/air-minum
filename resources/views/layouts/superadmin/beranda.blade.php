ea1198a1d7308b8b64bd8129a48c30578e5eb96b
dhani.frans <dhani.francisco.9@gmail.com>
2022-12-24 14:08:15 +0700

    perbaikan error js beranda superadmin

--- resources/views/layouts/superadmin/beranda.blade.php
+++ resources/views/layouts/superadmin/beranda.blade.php
@@ -448,7 +448,7 @@
     <!-- Charts JS
 	============================================ -->
     <script src="{{ asset('admin/js/charts/Chart.js') }}"></script>
-    <script src="{{ asset('admin/js/charts/bar-chart.js') }}"></script>
+    <!-- <script src="{{ asset('admin/js/charts/bar-chart.js') }}"></script> -->
     <!--  todo JS
 		============================================ -->
     <script src="{{ asset('admin/js/todo/jquery.todo.js') }}"></script>
@@ -471,36 +471,37 @@
 <script>
     if (window.innerWidth < 992) {
 
-// close all inner dropdowns when parent is closed
-document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
-  everydropdown.addEventListener('hidden.bs.dropdown', function () {
-    // after dropdown is hidden, then find all submenus
-      this.querySelectorAll('.submenu').forEach(function(everysubmenu){
-        // hide every submenu as well
-        everysubmenu.style.display = 'none';
-      });
-  })
-});
+        // close all inner dropdowns when parent is closed
+        document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+        everydropdown.addEventListener('hidden.bs.dropdown', function () {
+            // after dropdown is hidden, then find all submenus
+            this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+                // hide every submenu as well
+                everysubmenu.style.display = 'none';
+            });
+        })
+        });
 
-document.querySelectorAll('.dropdown-menu a').forEach(function(element){
-  element.addEventListener('click', function (e) {
-      let nextEl = this.nextElementSibling;
-      if(nextEl && nextEl.classList.contains('submenu')) {	
-        // prevent opening link if link needs to open dropdown
-        e.preventDefault();
-        if(nextEl.style.display == 'block'){
-          nextEl.style.display = 'none';
-        } else {
-          nextEl.style.display = 'block';
-        }
+        document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+        element.addEventListener('click', function (e) {
+            let nextEl = this.nextElementSibling;
+            if(nextEl && nextEl.classList.contains('submenu')) {	
+                // prevent opening link if link needs to open dropdown
+                e.preventDefault();
+                if(nextEl.style.display == 'block'){
+                nextEl.style.display = 'none';
+                } else {
+                nextEl.style.display = 'block';
+                }
 
-      }
-  });
-})
+            }
+        });
+        })
 }
 // end if innerWidth
-}); 
+
     </script>
+    @yield("script")
     
 </body>
 
